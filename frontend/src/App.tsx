f81a69d02d49dbc5814e85eac0d43e195a4564a2
Hassaanjbaig-code <hassaanb000@gmail.com>
2025-04-11 01:16:17 +0500

    Create a react with vite

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css"
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-3xl font-bold underline">Welcome to the Vite + React + TypeScript + Tailwind CSS + DaisyUI + React Router + Redux + Redux Toolkit + Axios + React Query App!</h1>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </header>
+    </div>
+  )
+}
+export default App
